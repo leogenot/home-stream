f96e7be44fa7f7c3609688de31333f4287a9bf00
Léo Genot <45610291+leogenot@users.noreply.github.com>
2025-09-22 17:10:41 +0200

    started bottom nav

--- app/components/SiteNavbar.vue
+++ app/components/SiteNavbar.vue
@@ -1,20 +1,28 @@
 <template>
-  <nav class="fixed bottom-0 left-0 h-10 w-full p-2">
-    <div class="inline-flex justify-between">
-      <div class="menu-entry text-xxs grid gap-1">
-        <div class="icon size-4"></div>
+  <nav class="fixed bottom-0 left-0 h-fit w-full bg-red-300 p-4">
+    <div class="inline-flex w-full items-center justify-between">
+      <div
+        class="menu-entry text-xxs flex flex-col items-center justify-center gap-1"
+      >
+        <div class="icon size-4 bg-blue-200"></div>
         <span>Music</span>
       </div>
-      <div class="menu-entry text-xxs grid gap-1">
-        <div class="icon size-4"></div>
+      <div
+        class="menu-entry text-xxs flex flex-col items-center justify-center gap-1"
+      >
+        <div class="icon size-4 bg-blue-200"></div>
         <span>Playlists</span>
       </div>
-      <div class="menu-entry text-xxs grid gap-1">
-        <div class="icon size-4"></div>
+      <div
+        class="menu-entry text-xxs flex flex-col items-center justify-center gap-1"
+      >
+        <div class="icon size-4 bg-blue-200"></div>
         <span>Upload</span>
       </div>
-      <div class="menu-entry text-xxs grid gap-1">
-        <div class="icon size-4"></div>
+      <div
+        class="menu-entry text-xxs flex flex-col items-center justify-center gap-1"
+      >
+        <div class="icon size-4 bg-blue-200"></div>
         <span>Profile</span>
       </div>
     </div>
