e503978d82e51059851545bbc062c1069907b2d0
Léo Genot <45610291+leogenot@users.noreply.github.com>
2025-10-02 14:00:16 +0200

    url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -90,7 +90,7 @@ export default defineNuxtConfig({
         { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
         { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
         { rel: 'manifest', href: '/site.webmanifest' },
-        { rel: 'canonical', href: process.env.BASE_URL || 'https://home-stream.fallwinter.dk' },
+        { rel: 'canonical', href: process.env.BASE_URL || 'https://home-stream.servemp3.com' },
         { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
         { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
         { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Doto:wght@100..900&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap' },
